b28cf52daa52d87a7d86faba668e7893e5afbeae
Konstantin <ownedbycat@mail.ru>
2025-02-26 21:30:44 +0500

    prod db

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
 // db.js
 const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root', // Замените на ваше имя пользователя
-    password: '', // Замените на ваш пароль
-    database: 'badum_kz' // Замените на название вашей базы данных
+    user: 'badumuser',           // Тот, что создали/назначили
+    password: 'SuperSecretPass', // Пароль
+    database: 'badum_kz'         // Название вашей БД
 });
+
 connection.connect(error => {
     if (error) throw error;
     console.log('Successfully connected to the database.');
 });
+
 module.exports = connection;
