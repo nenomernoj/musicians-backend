1ebcac30afa1d356e6b5b26a9653b5d89671ee26
Konstantin <ownedbycat@mail.ru>
2025-06-05 06:52:23 +0500

    fix db pass

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Nenomortyr1991#',
     database: 'badum_kz',
     waitForConnections: true,
     connectionLimit: 10,
